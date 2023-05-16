4d837743c9c5e9434855b2cdc6c262332c30f432
Mihai Idu <idumihai16@gmail.com>
2023-05-16 10:58:52 +0200

    Update mirroring.sh

--- mirroring.sh
+++ mirroring.sh
@@ -6,6 +6,10 @@
 ################################################################################################################################################################################################
 #!/bin/bash
 
+# read three numbers and assigned them to 3 vars
+read -p "Enter the path to your pull-secret.json : " n1
+read -p "Enter the OCP version you want to mirror : " n2
+
 # Preparing the global variables for this section
 export REGISTRY_NAME=${HOSTNAME_FQDN}
 export REGISTRY_NAMESPACE=olm-mirror
@@ -13,8 +17,8 @@ export LOCAL_REG="${REGISTRY_NAME}:5000"
 export LOCAL_REPO="ocp-release"
 export LOCAL_RELEASE_IMAGES_REPOSITORY="ocp4/openshift4-release-images"
 export ARCHITECTURE="x86_64"
-export PULLSECRET_FILE=/root/openshift_pull.json
-export OCP_VERSION="4.12.2"
+export PULLSECRET_FILE=$n1
+export OCP_VERSION="$n2"
 export UPSTREAM_REPO=$(curl -s https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_VERSION/release.txt | grep 'Pull From: quay.io' | awk -F ' ' '{print $3}')
 oc adm release mirror -a ${PULLSECRET_FILE} --from=$UPSTREAM_REPO --to-release-image=${LOCAL_REG}/${LOCAL_REPO}:${OCP_VERSION}-${ARCHITECTURE} --to=${LOCAL_REG}/${LOCAL_REPO}
 oc image mirror -a ${PULLSECRET_FILE} ${LOCAL_REG}/${LOCAL_REPO}:${OCP_VERSION}-${ARCHITECTURE} ${LOCAL_REG}/${LOCAL_RELEASE_IMAGES_REPOSITORY}:${OCP_VERSION}-${ARCHITECTURE}
